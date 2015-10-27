193fb23dfdb074740afe06067f2fce04a3e44340
fukayatsu <fukayatsu@gmail.com>
2015-10-27 16:57:18 +0900

    disable raven

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,7 @@
-Raven.configure do |config|
-  config.dsn = Figaro.env.raven_dsn
-  config.environments = %w[ production ]
-  config.current_environment = Rails.env
-  config.tags = { environment: Rails.env }
-  config.excluded_exceptions = ['Build::BuildError']
-end
+# Raven.configure do |config|
+#   config.dsn = Figaro.env.raven_dsn
+#   config.environments = %w[ production ]
+#   config.current_environment = Rails.env
+#   config.tags = { environment: Rails.env }
+#   config.excluded_exceptions = ['Build::BuildError']
+# end
